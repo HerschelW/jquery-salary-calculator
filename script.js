e66da6dc3ae986fa36529df23ee825ac0c6de06e
HerschelW <herschelw88@gmail.com>
2020-05-24 04:28:12 -0500

    employees arr update

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let employees = [
       lastName: "Stark",
       employeeID: "001",
       employeeTitle: "Iron Man",
-      employeeSalary: 100000,
+      employeeSalary: 70000,
    },
    {
       firstName: "Steve",
@@ -15,24 +15,66 @@ let employees = [
    },
    {
       firstName: "Thor",
-      lastName: "Something",
+      lastName: "Odinson",
       employeeID: "003",
       employeeTitle: "God of Thunder",
-      employeeSalary: 67000,
+      employeeSalary: 125000,
    },
    {
       firstName: "Bruce",
       lastName: "Banner",
       employeeID: "004",
       employeeTitle: "Hulk",
-      employeeSalary: 98000,
+      employeeSalary: 118000,
    },
    {
       firstName: "Natasha",
       lastName: "Romonov",
       employeeID: "005",
       employeeTitle: "Black Widow",
-      employeeSalary: 75000,
+      employeeSalary: 45000,
+   },
+   {
+      firstName: "Peter",
+      lastName: "Parker",
+      employeeID: "006",
+      employeeTitle: "Intern",
+      employeeSalary: 90000,
+   },
+   {
+      firstName: "James",
+      lastName: "Bond",
+      employeeID: "007",
+      employeeTitle: "Secret Agent Man",
+      employeeSalary: 50000,
+   },
+   {
+      firstName: "Clint",
+      lastName: "Barton",
+      employeeID: "008",
+      employeeTitle: "Hawkeye",
+      employeeSalary: 45000,
+   },
+   {
+      firstName: "T'Challa",
+      lastName: "son of T'Chaka",
+      employeeID: "009",
+      employeeTitle: "Black Panther",
+      employeeSalary: 105000,
+   },
+   {
+      firstName: "Wanda",
+      lastName: "Maximoff",
+      employeeID: "010",
+      employeeTitle: "Scarlett Witch",
+      employeeSalary: 110000,
+   },
+   {
+      firstName: "Carol",
+      lastName: "Danvers",
+      employeeID: "011",
+      employeeTitle: "Captain Marvel",
+      employeeSalary: 205000,
    },
 ];
 
@@ -55,13 +97,25 @@ $("#addButton").on("click", function () {
    let eID = $("#employeeID").val();
    let eTitle = $("#employeeTitle").val();
    let eSalary = parseInt($("#employeeSalary").val());
-   employees.push({
-      firstName: fName,
-      lastName: lName,
-      employeeID: eID,
-      employeeTitle: eTitle,
-      employeeSalary: eSalary,
-   });
+   if (
+      fName === "" ||
+      lName === "" ||
+      eID === "" ||
+      eTitle === "" ||
+      eSalary === ""
+   ) {
+      alert("Please fill out all inputs!");
+   } else if (eSalary != Number) {
+      alert("Annual Salary and ID must be numbers!");
+   } else {
+      employees.push({
+         firstName: fName,
+         lastName: lName,
+         employeeID: eID,
+         employeeTitle: eTitle,
+         employeeSalary: eSalary,
+      });
+   }
 
    clearTable();
    addRow();
