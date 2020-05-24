17a917c4322f31db61c77f42d1dfb01e686ab366
HerschelW <herschelw88@gmail.com>
2020-05-24 03:49:54 -0500

    Got so carried away with this one that I didn't commit nearly as much as I should have. Yes, I am aware that commit messages should be concise and descriptive :P

--- script.js
+++ script.js
@@ -1,25 +1,42 @@
-let cohorts = [
+let employees = [
    {
-      name: "Ada",
-      status: "Tier 2",
-      count: 13,
+      firstName: "Tony",
+      lastName: "Stark",
+      employeeID: "001",
+      employeeTitle: "Iron Man",
+      employeeSalary: 100000,
    },
    {
-      name: "Babbage",
-      status: "Tier 1",
-      count: 13,
+      firstName: "Steve",
+      lastName: "Rogers",
+      employeeID: "002",
+      employeeTitle: "Captain 'Murrica",
+      employeeSalary: 78000,
    },
    {
-      name: "Charlie",
-      status: "Tier 2",
-      count: 13,
+      firstName: "Thor",
+      lastName: "Something",
+      employeeID: "003",
+      employeeTitle: "God of Thunder",
+      employeeSalary: 67000,
    },
    {
-      name: "Delta",
-      status: "Tier 2",
-      count: 13,
+      firstName: "Bruce",
+      lastName: "Banner",
+      employeeID: "004",
+      employeeTitle: "Lead Somebody",
+      employeeSalary: 98000,
+   },
+   {
+      firstName: "Natasha",
+      lastName: "Romonov",
+      employeeID: "005",
+      employeeTitle: "Black Widow",
+      employeeSalary: 75000,
    },
 ];
+
+let rowElement = $("<tr class='employeeRow'></tr>");
 
 function clearTable() {
    $("tbody").empty();
@@ -27,32 +44,93 @@ function clearTable() {
 
 function setup() {
    clearTable();
-   let rowElement = $("<tr></tr>");
-   cohorts.forEach((element) => {
-      let rowElement = $("<tr class='cohortRow'></tr>");
+   addRow();
+   totalMonth();
+}
+
+// Add array object function
+$("#addButton").on("click", function () {
+   let fName = $("#firstName").val();
+   let lName = $("#lastName").val();
+   let eID = $("#employeeID").val();
+   let eTitle = $("#employeeTitle").val();
+   let eSalary = parseInt($("#employeeSalary").val());
+   employees.push({
+      firstName: fName,
+      lastName: lName,
+      employeeID: eID,
+      employeeTitle: eTitle,
+      employeeSalary: eSalary,
+   });
+
+   clearTable();
+   addRow();
+   totalMonth();
+
+   console.log(employees);
+});
+
+// Add Row function
+function addRow() {
+   employees.forEach((element) => {
+      let rowElement = $("<tr class='employeeRow'></tr>");
       rowElement.append(
          "<td>" +
-            element.name +
+            element.firstName +
             "</td><td>" +
-            element.count +
+            element.lastName +
             "</td><td>" +
-            element.status +
-            "</td><td><button class='myBtn'>Delete</button></td>"
+            element.employeeID +
+            "</td><td>" +
+            element.employeeTitle +
+            "</td><td>" +
+            element.employeeSalary +
+            "</td><td id='myBtn'><button>Delete</button></td>"
       );
       $("table tbody").append(rowElement);
-   });
 
-   $(".myBtn").on("click", function () {
-      $(this).parent().parent().remove();
-   });
-
-   $("#cohortAdd").on("click", function () {
-      console.log("test");
+      //   remove row/array object function
+      $(rowElement).on("click", "#myBtn", function (event) {
+         let del = $(this).parent();
+         del.fadeOut(function () {
+            del.remove();
+            totalMonth();
+            objRemove();
+         });
+      });
    });
 }
 
 $("document").ready(setup);
 
-function addInput() {
-   $("form").append();
+// add total function
+function totalMonth() {
+   let tables = document.getElementById("tableBody");
+   let sumVal = 0;
+
+   for (let i = 0; i < tables.rows.length; i++) {
+      let someSal = Number(tables.rows[i].cells[4].innerText);
+
+      sumVal += someSal;
+   }
+   totalMonthly = Math.round(sumVal / 12);
+   $("#total").text("Total Monthly: $" + totalMonthly);
+}
+
+// remove deleted object from array
+function objRemove() {
+   let tables = document.getElementById("tableBody");
+   let tempArr = [];
+   console.log(tables.rows.length);
+   for (let i = 0; i < tables.rows.length; i++) {
+      let id = tables.rows[i].cells[2].innerText;
+      console.log(id);
+      employees.forEach((element) => {
+         if (element.employeeID === id) {
+            tempArr.push(element);
+         }
+      });
+   }
+   employees = tempArr;
+   console.log(employees);
 }
